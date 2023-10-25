670864b8d580839c9a675106b16bf928e9f735d8
Hung.Thai (Patrick)/Sotatek/DnU <e.hung.thai@wesang.com>
2023-10-26 05:54:26 +0700

    Test Kafka work

---
+++ src/main/java/com/example/kafkademo/listener/StartupListener.java
@@ -0,0 +1,31 @@
+package com.example.kafkademo.listener;
+
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartupListener implements ApplicationRunner {
+  @Autowired private KafkaTemplate<String, String> kafkaTemplate;
+
+  public void sendMessage(String msg) {
+    kafkaTemplate.send("test", msg);
+  }
+
+  @KafkaListener(topics = "demo", groupId = "group-id")
+  public void listen(String message) {
+    System.out.println("Received Message in group - group-id: " + message);
+  }
+
+  @Override
+  public void run(ApplicationArguments args) throws Exception {
+    for (int i = 0; i < 1000; i++) {
+      sendMessage("Now: " + new Date());
+      Thread.sleep(2000);
+    }
+  }
+}
--- src/test/java/com/example/kafkademo/KafkaDemoApplicationTests.java
+++ src/test/java/com/example/kafkademo/KafkaDemoApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class KafkaDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
