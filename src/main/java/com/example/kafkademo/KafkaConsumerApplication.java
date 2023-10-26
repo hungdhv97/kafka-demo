547c075c4e0fa2271af659a8b55b9dd5b5df348f
Hung.Thai (Patrick)/Sotatek/DnU <e.hung.thai@wesang.com>
2023-10-26 07:16:00 +0700

    Fix application.yaml

--- src/main/java/com/example/kafkademo/KafkaConsumerApplication.java
+++ src/main/java/com/example/kafkademo/KafkaConsumerApplication.java
@@ -2,8 +2,10 @@ package com.example.kafkademo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example.kafkademo.consumer")
 public class KafkaConsumerApplication {
 
   public static void main(String[] args) {
